9ba5b84abdfb0bb756e74e7eb1e6b6c71f32128e
ipromise2324 <168allen55@gmail.com>
2023-11-17 20:51:34 +0800

    feat: add .env to prevent that url is showed

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
+import * as dotenv from "dotenv";
+dotenv.config();
+const eth_url = process.env.ETH_URL;
+const blockNumber = process.env.BLOCK_NUMBER;
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
     hardhat: {
       forking: {
-        url: "https://eth-mainnet.g.alchemy.com/v2/IlMIxZNyXEJCCpEp32QhrDX6OhGFHrBo",
-        blockNumber: 4501735 
+        url: eth_url!,
+        blockNumber: parseInt(blockNumber!),
       }
     }
   }
