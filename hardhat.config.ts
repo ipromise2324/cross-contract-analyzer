4d3e8438825daa49d92b737edf9189a68248f75f
ipromise2324 <168allen55@gmail.com>
2024-01-20 23:50:18 +0800

    feat: add TreasureDao attack

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,19 +1,20 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import * as dotenv from "dotenv";
-dotenv.config();
-const eth_url = process.env.ETH_URL;
-const blockNumber = process.env.BLOCK_NUMBER;
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import * as dotenv from 'dotenv'
+dotenv.config()
+const eth_url = process.env.ETH_URL
+const arbit_url = process.env.ARBIT_URL
+const blockNumber = process.env.BLOCK_NUMBER
 const config: HardhatUserConfig = {
-  solidity: "0.8.20",
-  networks: {
-    hardhat: {
-      forking: {
-        url: eth_url!,
-        blockNumber: parseInt(blockNumber!),
-      }
-    }
-  }
-};
+    solidity: '0.8.20',
+    networks: {
+        hardhat: {
+            forking: {
+                url: arbit_url!,
+                blockNumber: parseInt(blockNumber!),
+            },
+        },
+    },
+}
 
-export default config;
+export default config
