f2a2dbb1bde7df65ba3e8ff0cfbdbfe808417c16
ipromise2324 <168allen55@gmail.com>
2024-01-24 22:06:58 +0800

    feat: change comment to english

--- test/2022_10_17_ueri.ts
+++ test/2022_10_17_ueri.ts
@@ -9,10 +9,10 @@ describe('AttackContract Test', function () {
     let deployerSigner: any
 
     before(async function () {
-        // 获取部署者的签名者对象
+        // Get the deployer's signer object
         ;[deployerSigner] = await ethers.getSigners()
 
-        // 部署攻击者合约
+        // Deploy the attacker contract
         const AttackerContract = await ethers.getContractFactory('UeriAttack')
         attackerContract = await AttackerContract.deploy()
     })
@@ -23,20 +23,20 @@ describe('AttackContract Test', function () {
     })
 
     it('Should perform the attack', async function () {
-        // 获取攻击者合约的 WETH 余额
+        // Get the balance of WETH for the attacker contract
         WETH_TOKEN = await ethers.getContractAt('IERC20', '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2')
 
         const balanceBeforeAttack = await WETH_TOKEN.balanceOf(attackerContract.getAddress())
         console.log('Attacker WETH balance before attack:', ethers.formatEther(balanceBeforeAttack))
 
-        // 执行攻击
+        // Execute the attack
         const tx = await attackerContract.connect(deployerSigner).attack()
         await tx.wait()
 
         const balanceAfterAttack = await WETH_TOKEN.balanceOf(attackerContract.getAddress())
         console.log('Attacker WETH balance after attack:', ethers.formatEther(balanceAfterAttack))
 
-        // 检查攻击后余额是否增加（此行可选，视您的测试需求而定）
+        // Check if the balance after the attack has increased (this line is optional, depends on your test requirements)
         expect(balanceAfterAttack).to.be.gt(balanceBeforeAttack)
     })
 })
